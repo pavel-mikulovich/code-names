1b8d0e98f4e63aea422a5f17520f4b241148b5f8
Pavel Mikulovich <mikulovitch@gmail.com>
2019-07-19 20:25:13 +0300

    refactor

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = express();
 console.log('started');
 app.use(express.static('public'));
 
-// app.get('/', function (req, res) {
-//     res.send('Hello World!');
-// });
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 
-// app.listen(3000, function () {
-//     console.log('Example app listening on port 3000!');
-// });
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
