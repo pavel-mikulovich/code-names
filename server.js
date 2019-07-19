9d74b7888250e62cc2f225dfd0dcaf25fdc6f960
Pavel Mikulovich <mikulovitch@gmail.com>
2019-07-19 20:55:36 +0300

    use port from env vars so it can run on heroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = express();
 console.log('started');
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//     res.send('Hello World!');
+// });
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Example app listening on port 3000!');
+    // console.log('Example app listening on port 3000!');
 });
